99be962710fabdb437c2acc801a4ca25d96512b2
Tegan Lamoureux <tegan.lamoureux@remarkable.no>
2025-04-22 00:36:29 +0200

    Add start of keyboard map

--- lib/ogxc_osk/ogxc_osk.c
+++ lib/ogxc_osk/ogxc_osk.c
@@ -10,7 +10,7 @@ static SDL_Surface *screen_surface = 0;
 static SDL_Surface *background_surface = 0;
 static SDL_Window *draw_window = 0;
 static char final_string[256] = {0};
-static SDL_Color kb_font_color = {0x7F, 0x7F, 0x7F, 0xFF};
+static SDL_Color kb_font_color = {0x7F, 0xFF, 0x7F, 0xFF};
 
 /**
  * @brief Keyboard element, contains a character to display,
@@ -18,9 +18,9 @@ static SDL_Color kb_font_color = {0x7F, 0x7F, 0x7F, 0xFF};
  */
 typedef struct
 {
-    char c;
-    int x;
-    int y;
+    const char *c;
+    const int x;
+    const int y;
 } kb_elem;
 
 /**
@@ -30,15 +30,89 @@ typedef struct
  *
  * Clang format disabled for readability.
  */ // clang-format off
-#define LOWER_KB_MAP_SIZE 26
+ #define LOWER_KB_MAP_SIZE 30
+ #define UPPER_KB_MAP_SIZE 30
 
-#define KB_MAP_ROW_1_X 10
-#define KB_MAP_ROW_1_Y 10
-#define KB_MAP_ROW_1_PAD 10
+#define LKB_MAP_ROW_1_X 140
+#define LKB_MAP_ROW_1_Y 175
+#define LKB_MAP_ROW_2_X 140
+#define LKB_MAP_ROW_2_Y 205
+#define LKB_MAP_ROW_3_X 140
+#define LKB_MAP_ROW_3_Y 235
+
+#define UKB_MAP_ROW_1_X 140
+#define UKB_MAP_ROW_1_Y 285
+#define UKB_MAP_ROW_2_X 140
+#define UKB_MAP_ROW_2_Y 315
+#define UKB_MAP_ROW_3_X 140
+#define UKB_MAP_ROW_3_Y 345
+
+#define KB_MAP_ROW_PAD 25
 
 static kb_elem lower_kb_map[LOWER_KB_MAP_SIZE] = {
-    {'q', 100, 10},
+    {"q", (LKB_MAP_ROW_1_X + (0 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"w", (LKB_MAP_ROW_1_X + (1 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"e", (LKB_MAP_ROW_1_X + (2 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"r", (LKB_MAP_ROW_1_X + (3 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"t", (LKB_MAP_ROW_1_X + (4 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"y", (LKB_MAP_ROW_1_X + (5 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"u", (LKB_MAP_ROW_1_X + (6 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"i", (LKB_MAP_ROW_1_X + (7 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"o", (LKB_MAP_ROW_1_X + (8 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"p", (LKB_MAP_ROW_1_X + (9 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_1_Y},
+    {"a", (LKB_MAP_ROW_2_X + (0 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"s", (LKB_MAP_ROW_2_X + (1 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"d", (LKB_MAP_ROW_2_X + (2 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"f", (LKB_MAP_ROW_2_X + (3 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"g", (LKB_MAP_ROW_2_X + (4 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"h", (LKB_MAP_ROW_2_X + (5 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"j", (LKB_MAP_ROW_2_X + (6 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"k", (LKB_MAP_ROW_2_X + (7 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"l", (LKB_MAP_ROW_2_X + (8 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {".", (LKB_MAP_ROW_2_X + (9 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_2_Y},
+    {"z", (LKB_MAP_ROW_3_X + (0 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"x", (LKB_MAP_ROW_3_X + (1 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"c", (LKB_MAP_ROW_3_X + (2 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"v", (LKB_MAP_ROW_3_X + (3 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"b", (LKB_MAP_ROW_3_X + (4 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"n", (LKB_MAP_ROW_3_X + (5 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"m", (LKB_MAP_ROW_3_X + (6 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"@", (LKB_MAP_ROW_3_X + (7 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"/", (LKB_MAP_ROW_3_X + (8 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y},
+    {"\\", (LKB_MAP_ROW_3_X + (9 * KB_MAP_ROW_PAD)), LKB_MAP_ROW_3_Y}
+};
 
+static kb_elem upper_kb_map[UPPER_KB_MAP_SIZE] = {
+    {"Q", (UKB_MAP_ROW_1_X + (0 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"W", (UKB_MAP_ROW_1_X + (1 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"E", (UKB_MAP_ROW_1_X + (2 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"R", (UKB_MAP_ROW_1_X + (3 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"T", (UKB_MAP_ROW_1_X + (4 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"Y", (UKB_MAP_ROW_1_X + (5 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"U", (UKB_MAP_ROW_1_X + (6 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"I", (UKB_MAP_ROW_1_X + (7 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"O", (UKB_MAP_ROW_1_X + (8 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"P", (UKB_MAP_ROW_1_X + (9 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_1_Y},
+    {"A", (UKB_MAP_ROW_2_X + (0 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"S", (UKB_MAP_ROW_2_X + (1 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"D", (UKB_MAP_ROW_2_X + (2 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"F", (UKB_MAP_ROW_2_X + (3 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"G", (UKB_MAP_ROW_2_X + (4 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"H", (UKB_MAP_ROW_2_X + (5 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"J", (UKB_MAP_ROW_2_X + (6 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"K", (UKB_MAP_ROW_2_X + (7 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"L", (UKB_MAP_ROW_2_X + (8 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {".", (UKB_MAP_ROW_2_X + (9 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_2_Y},
+    {"Z", (UKB_MAP_ROW_3_X + (0 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"X", (UKB_MAP_ROW_3_X + (1 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"C", (UKB_MAP_ROW_3_X + (2 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"V", (UKB_MAP_ROW_3_X + (3 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"B", (UKB_MAP_ROW_3_X + (4 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"N", (UKB_MAP_ROW_3_X + (5 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"M", (UKB_MAP_ROW_3_X + (6 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"@", (UKB_MAP_ROW_3_X + (7 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"/", (UKB_MAP_ROW_3_X + (8 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y},
+    {"\\", (UKB_MAP_ROW_3_X + (9 * KB_MAP_ROW_PAD)), UKB_MAP_ROW_3_Y}
 };
 // clang-format on
 
@@ -49,9 +123,11 @@ static void draw_bg(void)
     SDL_BlitSurface(background_surface, NULL, screen_surface, NULL);
     SDL_UpdateWindowSurface(draw_window);
 
-    // render lower case keyboard map by default
+    // render keyboard map
     for (int key = 0; key < LOWER_KB_MAP_SIZE; key++)
-        ogxc_ttf_write(&lower_kb_map[key].c, lower_kb_map[key].x, lower_kb_map[key].y, &kb_font_color, 18);
+        ogxc_ttf_write(lower_kb_map[key].c, lower_kb_map[key].x, lower_kb_map[key].y, &kb_font_color, 18);
+    for (int key = 0; key < UPPER_KB_MAP_SIZE; key++)
+        ogxc_ttf_write(upper_kb_map[key].c, upper_kb_map[key].x, upper_kb_map[key].y, &kb_font_color, 18);
 }
 
 static void draw_text(void)
